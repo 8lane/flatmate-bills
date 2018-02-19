558b69bd4439784eda68066b5ffd526bf52113a6
Tom  Christian <tctc91@gmail.com>
2018-02-19 22:00:35 +0000

    get bills service spec

--- src/app/LatestBills/services/updateBill.js
+++ src/app/LatestBills/services/updateBill.js
@@ -9,9 +9,5 @@ export default (updatedBill, originalBill) => {
     firebase.database().ref('bills/' + updatedBill.id).set(updatedBill)
       .then(() => dispatch(Creators.updateBillSuccess(originalBill)))
       .catch(err => dispatch(Creators.updateBillFailure(err, originalBill)))
-
-    // firebase.database().ref('bills').once('value')
-    //   .then(snapshot => dispatch(Creators.updateBillSuccess(snapshot.val())))
-    //   .catch(err => dispatch(Creators.updateBillFailure(err)))
   }
 }
